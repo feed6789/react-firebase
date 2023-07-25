e82343cabd57370616044795cdd98bc985bccc46
Hung <feedrojout3@gmail.com>
2023-07-25 10:41:04 +0700

    haha

---
+++ src/retest.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+retest.propTypes = {
+    
+};
+
+function retest(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default retest;
